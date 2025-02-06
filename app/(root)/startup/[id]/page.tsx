b84c8296c306e28145ec09f2261a55c3b4c736b1
David Sutanto <hello.davidsutanto@gmail.com>
2025-02-06 14:24:11 +0800

    add pararel data fetching and remove sequential data fetching

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -1,6 +1,9 @@
 import { formatDate } from "@/lib/utils";
 import { client } from "@/sanity/lib/client";
-import { STARTUPS_BY_ID_QUERY } from "@/sanity/lib/queries";
+import {
+  PLAYLIST_BY_SLUG_QUERY,
+  STARTUPS_BY_ID_QUERY,
+} from "@/sanity/lib/queries";
 import Image from "next/image";
 import Link from "next/link";
 import { notFound } from "next/navigation";
@@ -8,6 +11,7 @@ import React, { Suspense } from "react";
 import markdownit from "markdown-it";
 import { Skeleton } from "@/components/ui/skeleton";
 import View from "@/components/View";
+import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
 
 export const experimental_ppr = true;
 const md = markdownit();
@@ -15,7 +19,21 @@ const md = markdownit();
 const page = async ({ params }: { params: Promise<{ id: string }> }) => {
   const id = (await params).id;
 
+  //Pararel Data Fetching
+  const [post, { select: editorPosts }] = await Promise.all([
+    client.fetch(STARTUPS_BY_ID_QUERY, { id }),
+    client.fetch(PLAYLIST_BY_SLUG_QUERY, {
+      slug: "editor-picks",
+    }),
+  ]);
+
+  //Sequential Data Fetching
+  /* 
   const post = await client.fetch(STARTUPS_BY_ID_QUERY, { id });
+  const { select: editorPosts } = await client.fetch(PLAYLIST_BY_SLUG_QUERY, {
+    slug: "editor-picks",
+  });
+  */
 
   if (!post) return notFound();
 
@@ -73,7 +91,17 @@ const page = async ({ params }: { params: Promise<{ id: string }> }) => {
 
         <hr className="divider" />
 
-        {/* TODO: EDITOR SELECTED STARTUPS */}
+        {editorPosts?.length > 0 && (
+          <div className="max-w-4xl mx-auto">
+            <p className="text-30-semibold">Editor Picks</p>
+            <ul className="mt-7 card_grid-sm">
+              {editorPosts.map((post: StartupTypeCard, i: number) => (
+                <StartupCard key={i} post={post} />
+              ))}
+            </ul>
+          </div>
+        )}
+
         <Suspense fallback={<Skeleton className="view_skeleton" />}>
           <View id={id} />
         </Suspense>
